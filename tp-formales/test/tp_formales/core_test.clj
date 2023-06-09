6ee98495437c57b4fe15f69d4f0490e7e64db6f9
Valentina Adelsflugel <101295561+valenadels@users.noreply.github.com>
2023-06-09 12:57:29 -0300

    Add files via upload

---
+++ tp-formales/test/tp_formales/core_test.clj
@@ -0,0 +1,37 @@
+(ns tp-formales.core-test
+  (:require [clojure.test :refer :all]
+            [tp-formales.core :refer :all]))
+
+(deftest variable-string?-test
+  (testing "Prueba de la funcion: variable-string?"
+    (is (= true (variable-string? "x"))))
+    (is (= false (variable-string? 1))))
+
+(deftest variable-integer?-test
+  (testing "Prueba de la funcion: variable-integer?"
+    (is (= true (variable-integer? 1))))
+    (is (= false (variable-integer? "x"))))
+
+(deftest eliminar-cero-decimal-test
+  (testing "Prueba de la funcion: eliminar-cero-decimal"
+    (is (= 1 (eliminar-cero-decimal 1.0))))
+    (is (= 1.1 (eliminar-cero-decimal 1.1)))
+    (is (= 'A (eliminar-cero-decimal 'A))))
+
+(deftest eliminar-cero-entero-test
+  (testing "Prueba de la funcion: eliminar-cero-entero"
+    (is (= " 0" (eliminar-cero-entero 0))))
+    (is (= " 1.5" (eliminar-cero-entero 1.5)))
+    (is (= " 1" (eliminar-cero-entero 1)))
+    (is (= "A" (eliminar-cero-entero 'A)))
+    (is (= nil (eliminar-cero-entero nil)))
+    (is (= " .5" (eliminar-cero-entero 0.5)))
+    (is (= "-.5" (eliminar-cero-entero -0.5))) 
+    (is (= "-1.5" (eliminar-cero-entero -1.5))))
+
+
+(deftest expandir-nexts-test
+  (let [sentences (list '(PRINT 1) (list 'NEXT 'A (symbol ",") 'B))
+        expected-result (list '(PRINT 1) (list 'NEXT 'A) (list 'NEXT 'B))
+        actual-result (expandir-nexts sentences)]
+    (is (= expected-result actual-result))))
